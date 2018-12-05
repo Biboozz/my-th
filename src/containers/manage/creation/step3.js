5d8c9985815a026a46acf15e1ed5d8f9acacf1b2
Thibaut Cens <thibaut.cens@gmail.com>
2018-12-05 13:47:23 -0600

    Step 3 shell done

--- src/containers/manage/creation/step3.js
+++ src/containers/manage/creation/step3.js
@@ -6,13 +6,15 @@ import {Col, Input, FormGroup, Label, Row, Form, Button} from "reactstrap";
 import ImageLoader from "react-load-image";
 import Spinner from 'react-spinner-material';
 import {Icon} from '@mdi/react'
-import {mdiRobot, mdiPlusCircle} from '@mdi/js'
+import {mdiRobot, mdiPlusCircle, mdiImage, mdiSkipPrevious, mdiSkipNext} from '@mdi/js'
 
 import '../../../styles/campaignCreation/step3.css'
 import '../../../styles/separator.css'
 import '../../../styles/utils.css'
 
 import BigButton from "../../../components/bigButton";
+import {Link} from "react-router-dom";
+import produce from "immer";
 
 const testAssociation = [
     {name: "Test1", cover: "https://picsum.photos/500/500/?image=1063", id: "idTest1"},
@@ -27,6 +29,7 @@ class CampaignCreationStep3 extends Component {
             startedloading: false,
             readyToRender: false,
             fetchedPages: 0,
+            prefill: props.location.state.prefill,
             loadingMessage: "Init",
             associationList: null
         }
@@ -83,6 +86,26 @@ class CampaignCreationStep3 extends Component {
             else
                 this.loadingPages();
     }
+
+    updateImageMatchPrefill = (id, event) => {
+        this.setState({
+            prefill: produce(this.state, (draftPrefill) => {
+                if (!draftPrefill.imageMatches[id])
+                    draftPrefill.imageMatches = {};
+                draftPrefill.imageMatches[id] = event.target.value
+            })
+        })
+    }
+
+    updatePrefill = (event) => {
+        this.setState({
+            prefill: produce(this.state.prefill, draftPrefill => {
+                draftPrefill[event.target.id] = event.target.value;
+                console.log(event.target.id, event.target.value);
+                console.log(draftPrefill);
+            })
+        });
+    };
 
     render() {
         let associaitonRender = [];
@@ -149,8 +172,9 @@ class CampaignCreationStep3 extends Component {
                         <div style={{marginBottom: '1em'}}>
                             <FormGroup>
                                 <Label for={"firstMessage"}>First Message *</Label>
-                                <Input type={"textarea"} id={"firstMessage"} name={"firstMessage"}
-                                       placeholder={"Explain to your fans what are the goal and the rule of your experience"}/>
+                                <Input type={"textarea"} id={"firstMessage"} name={"firstMessage"} required value={this.state.prefill.firstMessage}
+                                       onChange={this.updatePrefill}
+                                       placeholder={"Explain to your fans what are the goal and the rule of your experience."}/>
                             </FormGroup>
                             <span>Image with you first message (optional)</span><Icon path={mdiPlusCircle}
                                                                                       size={1} color={"#111111"}
@@ -158,15 +182,50 @@ class CampaignCreationStep3 extends Component {
                         </div>
                         <FormGroup>
                             <Label for={"analysisMessage"}>Analysis message *</Label>
-                            <Input type={"textarea"} id={"analysisMessage"} name={"analysisMessage"}
-                                   placeholder={"It appears every time when a user sent a message"}/>
+                            <Input type={"textarea"} id={"analysisMessage"} name={"analysisMessage"} required value={this.state.prefill.analysisMessage}
+                                   onChange={this.updatePrefill}
+                                   placeholder={"It appears every time when a user sent a message."}/>
                         </FormGroup>
                         <FormGroup>
-                            <Label for={"nomatchMessage"}>No Match Message *</Label>
+                            <Label for={"nomatchMessage"}>No Match Message</Label>
                             <Input type={"textarea"} id={"nomatchMessage"} name={"nomatchMessage"}
-                                   placeholder={"It appears every time when a user sent a message"}/>
+                                   onChange={this.updatePrefill}
+                                   placeholder={"It appears every time when a user sent an unknown pic."}/>
                         </FormGroup>
+                        <FormGroup>
+                            {/*TODO: Recover images from step2*/}
+                            <Label for={"matchMessage1"}>Match Message *</Label>
+                            <br/>
+                            <span><Icon path={mdiImage} size={2} color={"#000000"}/> Image name</span>
+                            <Input type={"textarea"} id={"matchMessage1"} name={"matchMessage1"} required
+                                   placeholder={"The above image is matched. Send a message relative to this image. For sweepstake experience, do not forget to inform your user that his participation is then validated."}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for={"defaultMessage"}>Default Message</Label>
+                            <Input type={"textarea"} id={"defaultMessage"} name={"defaultMessage"} value={this.state.prefill.defaultMessage}
+                                   onChange={this.updatePrefill}
+                                   placeholder={"It appears every time when the bot doesn't understand the user message. For example, if a user sends some text, the bot will use this default message to answer."}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for={"finalMessage"}>Final Message</Label>
+                            <Input type={"textarea"} id={"finalMessage"} name={"finalMessage"} value={this.state.prefill.finalMessage}
+                                   onChange={this.updatePrefill}
+                                   placeholder={"This message will come just after the Match message. It's dedicated for sharing the experience, a share button will be displayed."}/>
+                        </FormGroup>
+                        <div>
+                            <Link to={{pathname: `step2`, state: {prefill: this.state.prefill}}}>
+                                <Button className={"NavigationButton BackButton"}>
+                                    Back
+                                    <Icon path={mdiSkipPrevious} size={1}/>
+                                </Button>
+                            </Link>
+                            <Button className={"NavigationButton NextButton"}>
+                                Next
+                                <Icon path={mdiSkipNext} size={1}/>
+                            </Button>
+                        </div>
                     </Form>
+
                 </div>
             </div>
 
